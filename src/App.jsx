e5b61e87c0ff1f01cd0153c124283659d5a60c70
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-05-18 01:34:33 -0300

    primer ejercicio resuelto

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Ejercicio1 from "./components/Ejercicio1";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div>
-      <p>hola</p>
-    </div>
-  )
+    <Ejercicio1 />
+  );
 }
 
-export default App
+export default App;
